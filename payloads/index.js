e632ee639162cfc4540288fe037b393856094bd0
jekoned <44309345+jekoned@users.noreply.github.com>
2025-02-14 09:02:49 -0800

    Update index.js

--- payloads/index.js
+++ payloads/index.js
@@ -7,7 +7,7 @@ const uiTemplate = `
 // chrome.fileManagerPrivate.openURL();
 // }
 const managementTemplate = `
-<body style="background-image: url('https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png');background-color:black;">
+<body style="background-image: url('https://staticg.sportskeeda.com/editor/2024/02/70b2f-17090957549723-1920.jpg');background-color:black;">
 <div id="chrome_management_disable_ext">
 <h1 style="color:white; font-family:courier"> Everything has been disabled. You can say thanks to lebron for automating the process! </h1>
 <p style="color:white; font-family:courier"> ALL EXTENSIONS HAVE BEEN DISABLED. YOU CAN CLOSE THE PAGE </p>
