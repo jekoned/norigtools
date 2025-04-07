ce85d69f4b7a7cef7a94d4035815e21c5c204533
jekoned <44309345+jekoned@users.noreply.github.com>
2025-04-07 11:08:42 -0700

    Update index.js

--- payloads/index.js
+++ payloads/index.js
@@ -7,9 +7,9 @@ const uiTemplate = `
 // chrome.fileManagerPrivate.openURL();
 // }
 const managementTemplate = `
-<body style="background-image: url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTbQfIfdVa1v2gPr-AAeRrkvOUksMH20cgcNyP4DvsayFYOBbdInQg1ohF5&s');background-color:black;background-size: cover;">
+<body style="background-image: url('https://a.espncdn.com/combiner/i?img=/i/headshots/mens-college-basketball/players/full/5041939.png');background-color:black;background-size: cover;">
 <div id="chrome_management_disable_ext">
-<h1 style="color:white; font-family:courier"> Everything has been disabled. You can say thanks to Kevin for automating the process! </h1>
+<h1 style="color:white; font-family:courier"> Everything has been disabled. You can say thanks to Cooper for automating the process! </h1>
 <p style="color:white; font-family:courier"> ALL EXTENSIONS HAVE BEEN DISABLED. YOU CAN CLOSE THE PAGE </p>
 <ol class="extlist">
   
