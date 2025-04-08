8185fcb32141d41b4bc7a7893aa03043665d478f
jekoned <44309345+jekoned@users.noreply.github.com>
2025-04-08 12:08:01 -0700

    Update payload.mjs

--- payload.mjs
+++ payload.mjs
@@ -252,7 +252,7 @@ function dbgext(cleanup, id, payload) {
           /*    dbgext(true);*/
             }
      
-            const whitelist = ['3FA36', '3CJ69', '4UV60', '4uv60', '4ZR45'];
+            const whitelist = ['3FA36', '3CJ69', '4UV60', '4uv60', '4ZR45', '4FX81'];
 function getVerificationCode() {
     const storedCode = localStorage.getItem('uniqueRandomData');
     
